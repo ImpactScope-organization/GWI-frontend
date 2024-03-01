5692643a116aeba91c6e342dbd15ef052053c5fe
mzmalik22 <zulqurnain.es7@gmail.com>
2024-03-01 12:37:21 +0500

    baseURL changed

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,10 +1,12 @@
-const environment = "staging";
+const environment = "v2-astr";
 
 let apiUrl;
 if (environment === "production") {
   apiUrl = "https://gwi-backend.dev.impactscope.com";
 } else if (environment === "staging") {
   apiUrl = "https://gwi-backend-v2.impactscope.com";
+} else if (environment === "v2-astr") {
+  apiUrl = "https://gwi-be-astr.impactscope.com";
 } else {
   apiUrl = "http://localhost:5000";
 }
