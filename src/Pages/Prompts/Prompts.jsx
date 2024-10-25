ea956341456333632b44c2d5eb9e756c85e0ec08
Balazs Bereczky <bereczkybalazs1@gmail.com>
2024-10-25 10:46:36 +0200

    prompts block

--- src/Pages/Prompts/Prompts.jsx
+++ src/Pages/Prompts/Prompts.jsx
@@ -1,7 +1,13 @@
+import { PageContainer } from '../../Components/Page/PageContainer/PageContainer'
+import { PageHeader } from '../../Components/Page/PageHeader/PageHeader'
+import React from 'react'
+import { PageContentContainer } from '../../Components/Page/PageContentContainer/PageContentContainer'
+
 export const Prompts = () => {
   return (
-    <div>
-      <h1> Prompts </h1>
-    </div>
+    <PageContainer>
+      <PageHeader title="Prompts" subTitle="Overview all of prompts here"></PageHeader>
+      <PageContentContainer>Prompts</PageContentContainer>
+    </PageContainer>
   )
 }
