09d074cea1711f101046479762224e9f1e243e20
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-01-10 14:39:31 +0100

    cleanup

--- src/Pages/PromptCategories/EditPromptCategory/components/EditPromptSubCategoriesForm/EditPromptSubCategoriesForm.jsx
+++ src/Pages/PromptCategories/EditPromptCategory/components/EditPromptSubCategoriesForm/EditPromptSubCategoriesForm.jsx
@@ -16,33 +16,23 @@ export const EditPromptSubCategoriesForm = () => {
   return (
     <div className="w-full">
       <h2 className="text-darkBlack font-bold text-xl mb-4">Sub Categories</h2>
+      <div className="pb-1 w-full flex gap-2">
+        <Input placeholder="New category" value={newCategoryName} onChange={onCategoryNameChange} />
+        <Button type="default" icon={<PlusOutlined />} onClick={addSubCategory}>
+          Add item
+        </Button>
+      </div>
+      <Divider className="my-2" />
       <div>
-        <div className="bg-red-400 relative">
-          <div className={`w-full bg-white rounded absolute mt-2 z-10`}>
-            <div className="pb-1 w-full flex gap-2">
-              <Input
-                placeholder="New category"
-                value={newCategoryName}
-                onChange={onCategoryNameChange}
-              />
-              <Button type="default" icon={<PlusOutlined />} onClick={addSubCategory}>
-                Add item
-              </Button>
-            </div>
-            <Divider className="my-2" />
-            <div>
-              {subCategories &&
-                subCategories.map((item) => (
-                  <SubCategoryEditListItem
-                    key={item.id}
-                    item={item}
-                    refetchCategoryItems={refetchSubCategories}
-                    toggleDropdownVisible={() => {}}
-                  />
-                ))}
-            </div>
-          </div>
-        </div>
+        {subCategories &&
+          subCategories.map((item) => (
+            <SubCategoryEditListItem
+              key={item.id}
+              item={item}
+              refetchCategoryItems={refetchSubCategories}
+              toggleDropdownVisible={() => {}}
+            />
+          ))}
       </div>
     </div>
   )
