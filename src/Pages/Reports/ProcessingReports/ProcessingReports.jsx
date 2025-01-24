555e6aae548201350ca956231f6c3092186f8dc2
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-01-24 16:33:19 +0100

    report processing list

--- src/Pages/Reports/ProcessingReports/ProcessingReports.jsx
+++ src/Pages/Reports/ProcessingReports/ProcessingReports.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { ReportContainer } from '../components/ReportContainer/ReportContainer'
-import { useGetAllInitializedReports } from '../../../Hooks/reports-hooks'
-import { ReportList } from '../components/ReportList/ReportList'
+import { useQuery } from '@tanstack/react-query'
+import { fetchReportQueueList } from '../api/ReportQueueApi'
+import { ProcessingReportList } from './ProcessingReportList'
 
 export const ProcessingReports = () => {
-  const { data } = useGetAllInitializedReports()
+  const { data } = useQuery({
+    queryKey: ['fetchReportQueueList'],
+    queryFn: () => fetchReportQueueList()
+  })
 
   return (
     <ReportContainer>
-      <ReportList data={data} />
+      <ProcessingReportList data={data} />
     </ReportContainer>
   )
 }
