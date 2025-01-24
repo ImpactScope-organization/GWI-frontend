0cb1a167001124b202ff51ac58377cc49b675d41
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-01-24 16:38:04 +0100

    back button

--- src/Pages/Reports/ProcessingDetailsReport/ProcessingDetailsReport.jsx
+++ src/Pages/Reports/ProcessingDetailsReport/ProcessingDetailsReport.jsx
@@ -3,12 +3,15 @@ import { PageContainer } from '../../../Components/Page/PageContainer/PageContai
 import { useProcessingDetailsReport } from './useProcessingDetailsReport'
 import { Progress } from 'antd'
 import { PageHeader } from '../../../Components/Page/PageHeader/PageHeader'
+import { BackButtonLink } from '../../../Components/BackButtonLink/BackButtonLink'
+import { ROUTES } from '../../../routes'
 
 export const ProcessingDetailsReport = () => {
   const { percentage, processText } = useProcessingDetailsReport()
 
   return (
     <PageContainer>
+      <BackButtonLink to={ROUTES.reports.processing} />
       <PageHeader title="Processing companyName" subTitle={processText} />
       <div className="flex flex-col items-center justify-center h-full gap-4">
         <Progress percent={percentage} format={(percent) => `${percent}%`} />
