f83d43ab21ac77319fa6a3d60a96124b4fee5a91
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-01-27 08:17:52 +0100

    set base loading state

--- src/Hooks/useLoading.js
+++ src/Hooks/useLoading.js
@@ -1,7 +1,7 @@
 import { useCallback, useState } from 'react'
 
 export const useLoading = () => {
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
 
   const startLoading = useCallback(() => setIsLoading(true), [])
   const finishLoading = useCallback(() => setIsLoading(false), [])
