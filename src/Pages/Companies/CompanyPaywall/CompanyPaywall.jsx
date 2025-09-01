58177dffa48c943a2abcb440501f6f643f860f7b
Balazs Bereczky <bereczkybalazs1@gmail.com>
2025-09-01 17:37:17 +0200

    list item style

--- src/Pages/Companies/CompanyPaywall/CompanyPaywall.jsx
+++ src/Pages/Companies/CompanyPaywall/CompanyPaywall.jsx
@@ -1,13 +1,25 @@
-import { PageHeader } from '../../../Components/Page/PageHeader/PageHeader'
 import React from 'react'
 import { PageContainer } from '../../../Components/Page/PageContainer/PageContainer'
-import { TitleWithBackButton } from '../../../Components/TitleWithBackButton/TitleWithBackButton'
 import { ROUTES } from '../../../routes'
+import { CompanyListItemPaywall } from '../components/CompanyListItemPaywall/CompanyListItemPaywall'
+import { PageHeaderWithBackButton } from '../../../Components/Page/PageHeaderWithBackButton/PageHeaderWithBackButton'
+import { useGetCompany } from '../api/CompanyApiQuery'
 
 export const CompanyPaywall = () => {
+  const { company } = useGetCompany()
+
   return (
     <PageContainer>
-      <TitleWithBackButton title={`company name`} to={ROUTES.companies.index} />
+      <PageHeaderWithBackButton
+        title={`Unlock ${company?.name}`}
+        subTitle="Get instant access to in-depth ESG reports that reveal the true impact of each company."
+        to={ROUTES.companies.index}
+      />
+      <div className="flex justify-center w-full">
+        <div className="w-2/3 mt-12">
+          <CompanyListItemPaywall company={company} disabled />
+        </div>
+      </div>
     </PageContainer>
   )
 }
--- src/Pages/Companies/components/CompanyListItemPaywall/CompanyListItemPaywall.jsx
+++ src/Pages/Companies/components/CompanyListItemPaywall/CompanyListItemPaywall.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import { CategorizedListItemLink } from '../../../../Components/CategorizedList/CategorizedListItemLink/CategorizedListItemLink'
-import { CategorizedListItemDate } from '../../../../Components/CategorizedList/CategorizedListItemLink/CategorizedListItemDate'
-import { handleDateFormat } from '../../../../utils/date'
 import { CategorizedListItemTitle } from '../../../../Components/CategorizedList/CategorizedListItemLink/CategorizedListItemTitle'
 import { CategorizedListItemCategoryContainer } from '../../../../Components/CategorizedList/CategorizedListItemLink/CategorizedListItemCategoryContainer'
 import { CategorizedListItemCategory } from '../../../../Components/CategorizedList/CategorizedListItemLink/CategorizedListItemCategory'
 import { StarFilled } from '@ant-design/icons'
 import { getRouteWithParams, ROUTES } from '../../../../routes'
 
-export const CompanyListItemPaywall = ({ company }) => {
+export const CompanyListItemPaywall = ({ company, disabled = false }) => {
   return (
     <CategorizedListItemLink
-      to={getRouteWithParams(ROUTES.companies.paywall, {
-        companyId: company._id
-      })}
+      to={
+        !disabled
+          ? getRouteWithParams(ROUTES.companies.paywall, {
+              companyId: company.companyId
+            })
+          : ''
+      }
     >
       <div className="relative">
         <div className="absolute right-0 text-primary">
           <StarFilled />
         </div>
-        <CategorizedListItemDate>{handleDateFormat(company?.createdAt)}</CategorizedListItemDate>
         <CategorizedListItemTitle>{company?.name}</CategorizedListItemTitle>
         <CategorizedListItemCategoryContainer>
           <div>Jurisdiction:</div>
