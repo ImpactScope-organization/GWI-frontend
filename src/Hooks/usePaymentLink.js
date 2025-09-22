e5bd26b6d94fd2fcf9dced108b29730c1e6d68ef
balazs-impact-scope <balazs@impactscope.com>
2025-09-22 18:13:03 +0200

    wip

--- src/Hooks/usePaymentLink.js
+++ src/Hooks/usePaymentLink.js
@@ -6,6 +6,7 @@ export const usePaymentLink = () => {
   const {
     userInfo: { email, id }
   } = useAuthContext()
+
   const paymentLink = useMemo(() => {
     return `${config.stripeQuarterlyProductPaymentUrl}?prefilled_email=${email}&client_reference_id=${id}`
   }, [email, id])
