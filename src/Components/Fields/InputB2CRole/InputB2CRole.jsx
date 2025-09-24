0948a1a73f96aa0532f90bb099f17fc39302ef1c
balazs-impact-scope <balazs@impactscope.com>
2025-09-24 15:52:16 +0200

    use input b2c role

--- src/Components/Fields/InputB2CRole/InputB2CRole.jsx
+++ src/Components/Fields/InputB2CRole/InputB2CRole.jsx
@@ -1,40 +1,11 @@
 import { useFormikContext } from 'formik'
 import { Input, Select } from 'antd'
-import { useCallback, useEffect, useMemo, useState } from 'react'
-import {
-  B2C_ROLES,
-  DEFINED_B2C_ROLES_WITH_LABEL,
-  B2C_ROLES_WITH_LABEL_AND_OPTION_OTHER
-} from './b2cRoles'
+import { B2C_ROLES_WITH_LABEL_AND_OPTION_OTHER } from './b2cRoles'
+import { useInputB2CRole } from './useInputB2CRole'
 
 export const InputB2CRole = ({ name, disabled = false }) => {
   const formik = useFormikContext()
-  const [b2cRole, setB2CRole] = useState(undefined)
-
-  const isDefinedRole = useCallback((value) => {
-    return DEFINED_B2C_ROLES_WITH_LABEL.some(({ value: roleValue }) => roleValue === value)
-  }, [])
-
-  useEffect(() => {
-    if (!b2cRole) {
-      setB2CRole(formik.values[name])
-    }
-  }, [b2cRole, formik.values, name])
-
-  const setRole = useCallback(
-    async (value) => {
-      if (isDefinedRole(value) || value === B2C_ROLES.OTHER) {
-        setB2CRole(value)
-      }
-      await formik.setFieldValue(name, value)
-    },
-    [formik, isDefinedRole, name]
-  )
-
-  const isOtherInputVisible = useMemo(
-    () => !isDefinedRole(formik.values[name]),
-    [formik.values, isDefinedRole, name]
-  )
+  const { b2cRole, setRole, isOtherInputVisible } = useInputB2CRole({ name })
 
   return (
     <div className="w-full">
