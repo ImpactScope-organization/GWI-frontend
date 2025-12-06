c3f77ffcfe79e575aca285653ab496d27147d2fb
hm92 <humam.maulana92@gmail.com>
2025-12-06 15:42:16 +0700

    fix(registrationForm): GWI-124 change company type registration to select

--- src/Pages/B2C/Register/Register.jsx
+++ src/Pages/B2C/Register/Register.jsx
@@ -5,6 +5,8 @@ import { Form, FormikProvider } from 'formik'
 import { InputPassword } from '../../../Components/Fields/InputPassword'
 import { FilledSuccessButton } from '../../../Components/Buttons/FilledSuccessButton'
 import { InputB2CRole } from '../../../Components/Fields/InputB2CRole/InputB2CRole'
+import React from 'react'
+import { Select } from 'antd'
 
 export const Register = () => {
   const { registerFormik, isLoading } = useRegister()
@@ -19,7 +21,45 @@ export const Register = () => {
             <InputPassword name="password" label="Password" />
             <InputPassword name="passwordAgain" label="Password Again" />
             <InputText name="companyName" label="Company Name" />
-            <InputText name="companyType" label="Company Type" />
+            <div>
+              <label className="text-md text-darkBlack mb-1 font-semibold block">
+                Company Type
+              </label>
+              <Select
+                name="companyType"
+                placeholder="Select Company Type"
+                optionFilterProp="label"
+                onChange={(value) => registerFormik.setFieldValue('companyType', value)}
+                status={
+                  registerFormik.touched.companyType && registerFormik.errors.companyType
+                    ? 'error'
+                    : 'success'
+                }
+                options={[
+                  {
+                    value: 'financialRegulator',
+                    label: 'Financial Regulator'
+                  },
+                  {
+                    value: 'publiclyListedCompany',
+                    label: 'Publicly listed company'
+                  },
+                  {
+                    value: 'advertisingWatchdog',
+                    label: 'Advertising watchdog'
+                  },
+                  {
+                    value: 'ngo',
+                    label: 'NGO'
+                  },
+                  {
+                    value: 'other',
+                    label: 'Other'
+                  }
+                ]}
+                className="w-full"
+              />
+            </div>
             <InputB2CRole name="b2cRole" />
             <FilledSuccessButton type="submit" isLoading={isLoading}>
               Sign up
